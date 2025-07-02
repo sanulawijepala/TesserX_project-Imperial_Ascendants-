25adf980da2c8fc239a2fbcb889d02a6bbd8dc51
sanulawijepala <147908304+sanulawijepala@users.noreply.github.com>
2025-07-02 16:30:29 +0530

    Update code.ino
    discarded the need for int overflow prevention

--- Drainage System/code.ino
+++ Drainage System/code.ino
@@ -1,16 +1,19 @@
-#include <Servo.h>
+#include <ESP32Servo.h>
 
 Servo Valve;//creates the servo object for the valve
 
 int WL_start ;
 int WL_current ;
 int Time; 
-int checkdelay = 1000 ;
+int delayt = 1000 ;
 bool flood = 0 ; //this value is whether the system is in a flood state or not
+
+bool constate = 0;//the state of the connectivity of the master controller
 
 int SPKR = 3 ;//this is a PWM pin and is used for the speaker 
 int DrnVlv_pos = 0 ; // used to control the servo in the drainage valve
 int pitch = 1000 ; //the pitch of the alarm
+int led = 4 ;//led pin (turns on in a flood)
 
 int id = 0 ; //id of the device in the network
 
@@ -41,8 +44,6 @@ void loop() {
     
     }
   WL_current = analogRead(A0);
-
-  if (Time % checkdelay == 0) {
     
     if (WL_current > WL_start) {
       
@@ -52,8 +53,10 @@ void loop() {
       tone(SPKR , pitch ); //turns on the alarm
       
       flood = 1 ; // a flood is underway
+
+      digital.Write(led , 1);//turn on LED
       
-      }
+    }
      if (flood == 1) {
 
       Serial.print("Flood is happening");//sends "a flood is happening" signal the master controller
@@ -61,18 +64,19 @@ void loop() {
       }
      else {
       Serial.println((String)"No."+ id + " is alive.");//periodicly checks connectivity with the master controller and keepalive 
+      State = Serial.read();
 
       if (DrnVlv_pos == 90){// checks whether the valve is engaged and if it is then disengages it
         
         DrnVlv_pos = 0;
         Valve.write(DrnVlv_pos);
+        digital.Write(led , 0);//turns off the LED after flood
         
         }
       }
-    }
   
 
   Time =+ 1 ;//increments the time each cycle
   
-  delay(1); // I didn't know the clock speed so i just made a delay of 1 ms making the program repeat at a rate of 1000Hz
+  delay(delayt); // I didn't know the clock speed so i just made a delay of 1 ms making the program repeat at a rate of 1000Hz
 }
